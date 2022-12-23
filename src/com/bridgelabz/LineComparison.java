a3209457b1c376448bc806d9ca8dcd2075d0faaf
Your Name <you@example.com>
2022-12-23 17:24:41 +0530

    comparing one line is greater or lesser or equal to another line

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -17,8 +17,8 @@ public class LineComparison {
         int x2 = sc.nextInt();
         System.out.println("Enter second number in second point: ");
         int y2 = sc.nextInt();
-        double x = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
-        System.out.println("Length of first line is: "+x);
+        double x = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        String len1 = String.valueOf(x);
 
         System.out.println("Enter first point coordinates of first line");
         System.out.println("Enter first number in first point: ");
@@ -30,12 +30,17 @@ public class LineComparison {
         int a2 = sc.nextInt();
         System.out.println("Enter second number in second point: ");
         int b2 = sc.nextInt();
-        double a = Math.sqrt(Math.pow((a2 - a1),2) + Math.pow((b2 - b1),2));
-        System.out.println("Length of second line is: "+a);
-        if(Objects.equals(x, a) == true){
+        double y = Math.sqrt(Math.pow((a2 - a1), 2) + Math.pow((b2 - b1), 2));
+        String len2 = String.valueOf(y);
+        System.out.println("Length of first line is: " + len1);
+        System.out.println("Length of second line is: " + len2);
+        if(len1.compareTo(len2) == 0){
             System.out.println("Lines are equal");
+        }else if(len1.compareTo(len2) > 0){
+            System.out.println("First line length "+ len1 +" is greater than second line "+ len2);
         }else{
-            System.out.println("Lines are not equal");
+            System.out.println("Second line length "+ len2 + " is greater than first line "+ len1);
         }
     }
+
 }
