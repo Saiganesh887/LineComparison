a15408f15735ddd3738182ecc332ce767e459053
Your Name <you@example.com>
2022-12-23 16:29:40 +0530

    comparing 2 lines equal or not

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,12 +1,13 @@
 package com.bridgelabz;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first point coordinates");
+        System.out.println("Enter first point coordinates of first line");
         System.out.println("Enter first number in first point: ");
         int x1 = sc.nextInt();
         System.out.println("Enter second number in first point: ");
@@ -17,6 +18,24 @@ public class LineComparison {
         System.out.println("Enter second number in second point: ");
         int y2 = sc.nextInt();
         double x = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
-        System.out.println("Length of line is: "+x);
+        System.out.println("Length of first line is: "+x);
+
+        System.out.println("Enter first point coordinates of first line");
+        System.out.println("Enter first number in first point: ");
+        int a1 = sc.nextInt();
+        System.out.println("Enter second number in first point: ");
+        int b1 = sc.nextInt();
+        System.out.println("Enter second point coordinates");
+        System.out.println("Enter first number in second point: ");
+        int a2 = sc.nextInt();
+        System.out.println("Enter second number in second point: ");
+        int b2 = sc.nextInt();
+        double a = Math.sqrt(Math.pow((a2 - a1),2) + Math.pow((b2 - b1),2));
+        System.out.println("Length of second line is: "+a);
+        if(Objects.equals(x, a) == true){
+            System.out.println("Lines are equal");
+        }else{
+            System.out.println("Lines are not equal");
+        }
     }
 }
