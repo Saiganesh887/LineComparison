d87211caf5d45ca26a3b851bd4d46c1e0b5e36eb
Your Name <you@example.com>
2023-01-04 17:41:47 +0530

    line comparision with oops

---
+++ src/com/bridgelabz/Line1.java
@@ -0,0 +1,47 @@
+package com.bridgelabz;
+
+public class Line1 {
+    public int getX1() {
+        return x1;
+    }
+
+    public int setX1(int x1) {
+        this.x1 = x1;
+        return x1;
+    }
+
+    private int x1;
+
+    public int getY1() {
+        return y1;
+    }
+
+    public int setY1(int y1) {
+        this.y1 = y1;
+        return y1;
+    }
+
+    private int y1;
+
+    public int getX2() {
+        return x2;
+    }
+
+    public int setX2(int x2) {
+        this.x2 = x2;
+        return x2;
+    }
+
+    private int x2;
+
+    public int getY2() {
+        return y2;
+    }
+
+    public int setY2(int y2) {
+        this.y2 = y2;
+        return y2;
+    }
+
+    private int y2;
+}
--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,46 +1,60 @@
 package com.bridgelabz;
 
-import java.util.Objects;
 import java.util.Scanner;
 
-public class LineComparison {
+public class LineComparison extends Line1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first point coordinates of first line");
-        System.out.println("Enter first number in first point: ");
-        int x1 = sc.nextInt();
-        System.out.println("Enter second number in first point: ");
-        int y1 = sc.nextInt();
-        System.out.println("Enter second point coordinates");
-        System.out.println("Enter first number in second point: ");
-        int x2 = sc.nextInt();
-        System.out.println("Enter second number in second point: ");
-        int y2 = sc.nextInt();
-        double x = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        String len1 = String.valueOf(x);
-
-        System.out.println("Enter first point coordinates of first line");
+        Line1 obj1 = new Line1();
         System.out.println("Enter first number in first point: ");
         int a1 = sc.nextInt();
+        obj1.getX1();
+        int x1 = obj1.setX1(a1);
         System.out.println("Enter second number in first point: ");
         int b1 = sc.nextInt();
+        obj1.getY1();
+        int y1 = obj1.setY1(b1);
         System.out.println("Enter second point coordinates");
         System.out.println("Enter first number in second point: ");
         int a2 = sc.nextInt();
+        obj1.getX2();
+        int x2 = obj1.setX2(a2);
         System.out.println("Enter second number in second point: ");
         int b2 = sc.nextInt();
-        double y = Math.sqrt(Math.pow((a2 - a1), 2) + Math.pow((b2 - b1), 2));
-        String len2 = String.valueOf(y);
-        System.out.println("Length of first line is: " + len1);
-        System.out.println("Length of second line is: " + len2);
+        obj1.getY2();
+        int y2 = obj1.setY2(b2);
+        double length1 = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        String len1 = String.valueOf(length1);
+
+        System.out.println("Enter first point coordinates of Second line");
+        System.out.println("Enter first number in first point: ");
+        Line1 obj2 = new Line1();
+        int p1 = sc.nextInt();
+        obj2.getX1();
+        int r1 = obj2.setX1(p1);
+        System.out.println("Enter second number in first point: ");
+        int q1 = sc.nextInt();
+        obj1.getY1();
+        int s1 = obj1.setY1(q1);
+        System.out.println("Enter second point coordinates");
+        System.out.println("Enter first number in second point: ");
+        int p2 = sc.nextInt();
+        obj1.getX2();
+        int r2 = obj1.setX2(p2);
+        System.out.println("Enter second number in second point: ");
+        int q2 = sc.nextInt();
+        obj1.getY2();
+        int s2 = obj1.setY2(q2);
+        double length2 = Math.sqrt(Math.pow((r2 - r1), 2) + Math.pow((s2 - s1), 2));
+        String len2 = String.valueOf(length2);
         if(len1.compareTo(len2) == 0){
             System.out.println("Lines are equal");
         }else if(len1.compareTo(len2) > 0){
-            System.out.println("First line length "+ len1 +" is greater than second line "+ len2);
+            System.out.println("First line length "+ length1 +" is greater than second line "+ length2);
         }else{
-            System.out.println("Second line length "+ len2 + " is greater than first line "+ len1);
+            System.out.println("Second line length "+ length2 + " is greater than first line "+ length1);
         }
     }
-
 }
